714488888317d9c5e852ac978c0aa3fcca9b8b51
Isaias Santos Silva <isaias.silva.dev@gmail.com>
2026-01-07 22:35:53 -0300

    visibilidade senha e responsividade

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,25 @@
 const botaoTema = document.getElementById("botaotema");
 const ico = botaoTema.querySelector("i");
+const senha = document.getElementById("password");
+const botao = document.getElementById("iconesenha");
+const icone = botao.querySelector("i");
+
+botao.addEventListener("click", () => {
+  const isPassword = senha.type === "password";
+
+  // alterna visibilidade
+  senha.type = isPassword ? "text" : "password";
+
+  // troca o ícone
+  icone.classList.toggle("bi-lock-fill");
+  icone.classList.toggle("bi-eye-fill");
+
+  // acessibilidade
+  botao.setAttribute(
+    "aria-label",
+    isPassword ? "Ocultar senha" : "Mostrar senha"
+  );
+});
 
 function aplicarTema(tema) {
   document.body.classList.toggle("claro", tema === "claro");
